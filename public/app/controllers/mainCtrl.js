3105c3d66ca931bacd9f99e17e9afefd53494e19
Mike Ulvila <mikeulvila@me.com>
2016-03-16 15:54:27 -0500

    account template displaying user name, id, access_token

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,9 +1,8 @@
 // public/app/controllers/MainCtrl.js
 angular.module('MainCtrl', []).controller('MainController', function($scope, $state, User) {
 
-    const UserObj = User.getUserObj().then((user) => {
-
-    console.log('UserObj>>>', user);
-    })
+    User.getUserObj().then((user) => {
+      $scope.user = user.data;
+    });
 
 });
