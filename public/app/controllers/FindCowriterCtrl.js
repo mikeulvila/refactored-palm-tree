e173c039eed0ba2e3ffa71b836b4c671924e6349
Mike Ulvila <mikeulvila@me.com>
2016-03-29 10:44:24 -0500

    added warning message for find-cowriter if user has not uploaded any songs

--- public/app/controllers/FindCowriterCtrl.js
+++ public/app/controllers/FindCowriterCtrl.js
@@ -3,6 +3,8 @@ angular.module('Capstone')
     function($scope, $state, User, Cowriter, Tracks) {
 
       $scope.nomore;
+      $scope.showWidget = false;
+      $scope.checkForTracks = false;
 
       var cowritersArray;
       var maxIndex;
@@ -15,13 +17,16 @@ angular.module('Capstone')
           $scope.cowriter = cowritersArray[0];
           Tracks.getTracks($scope.cowriter._id)
             .then(function(tracks) {
+              if (tracks.data.length === 0) {
+                $scope.checkForTracks = true;
+              }
+              console.log('tracks>>>', tracks.data.length)
               $scope.cowriter['tracks'] = tracks.data;
-              console.log('$scope.cowriter>>>', $scope.cowriter);
             }).catch(function(error) {
-              console.log('getTracks error>>>', error);
+              console.log(error);
             })
         }).catch(function(error) {
-          console.log('error>>>', error);
+          console.log(error);
         });
 
       var index = 1;
@@ -53,12 +58,23 @@ angular.module('Capstone')
               $scope.next();
             }
           }).catch(function(error) {
-            console.log('like error>>>', error);
+            console.log(error);
           });
-      }
+      };
+
+      // display only true genres
+      $scope.filterGenres = function(genres) {
+          var result = [];
+          angular.forEach(genres, function(value, key) {
+              if (value) {
+                  result.push(key);
+              }
+          });
+          return result;
+      };
 
       $scope.play = function (src) {
-        console.log('play function', src);
+        $scope.showWidget = true;
         $scope.iframeSrc = 'https://w.soundcloud.com/player/?auto_play=true&show_user=false&show_artwork=false&url='+src;
       };
 
