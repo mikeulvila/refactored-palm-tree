57c7a848706f02aaf0798e13bbd23dd7d974b139
Mike Ulvila <mikeulvila@me.com>
2016-04-06 08:30:18 -0500

    cleaned up user route and controller

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,7 +1,32 @@
 'use strict';
 const request = require('request');
-// Users model
+const _ = require('lodash');
+// MODEL
 const User = require('../models/User.js');
+
+module.exports.ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) { return next(); }
+  // else send 401 unauthorized
+  res.status(401).json('Please connect with SoundCloud');
+};
+
+module.exports.getUser = (req, res) => {
+  res.json(req.user);
+};
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.id, (err, user) => {
+    if (err) throw err;
+    res.json(user);
+  });
+};
+
+module.exports.updateUser = (req, res) => {
+  User.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, (err, user) => {
+    if (err) throw err;
+    res.sendStatus(200);
+  });
+};
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,49 +1,16 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const request = require('request');
-const _ = require('lodash');
 // controller
-const userCtrl = require('../controllers/userCtrl.js');
-// MODEL
-const User = require('../models/User.js');
-// env
-const CLIENT_ID = process.env.SOUNDCLOUD_CLIENT_ID;
-const CLIENT_SECRET = process.env.SOUNDCLOUD_CLIENT_SECRET;
-
-// GET     /forums              ->  index
-// GET     /forums/new          ->  new
-// POST    /forums              ->  create
-// GET     /forums/:forum       ->  show
-// GET     /forums/:forum/edit  ->  edit
-// PUT     /forums/:forum       ->  update
-// DELETE  /forums/:forum       ->  destroy
-
-// get user by id
-router.get('/user/:id', (req, res) => {
-  User.findById(req.params.id, (err, user) => {
-    if (err) throw err;
-    res.json(user);
-  });
-});
+const ctrl = require('../controllers/userCtrl.js');
 
 // GET USER OBJECT
-router.get('/user', ensureAuthenticated, (req, res) => {
-  res.json(req.user);
-});
+router.get('/user', ctrl.ensureAuthenticated, ctrl.getUser);
+
+// get user by id
+router.get('/user/:id', ctrl.getUserById);
 
 // UPDATE USER OBJECT
-router.put('/user/:id', (req, res) => {
-  User.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, (err, user) => {
-    if (err) throw err;
-    res.sendStatus(200);
-  });
-});
-
-function ensureAuthenticated (req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  // else send 401 unauthorized
-  res.status(401).json('Please connect with SoundCloud');
-};
+router.put('/user/:id', ctrl.updateUser);
 
 module.exports = router;
